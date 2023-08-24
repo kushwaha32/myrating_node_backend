a229bd6162f6dab23d9f8e9aeb7aa8736b8f5cc5
Raj Kushwaha <raj639kushwaha@gmail.com>
2023-08-24 18:39:59 +0530

    m

--- backend/app.js
+++ backend/app.js
@@ -31,8 +31,8 @@ app.use(express.static(`${__dirname}/public`));
 // 1) GLOBAL MIDDLEWARES
 // prevent cross site scription
 const corsOpts = {
-  // origin: "http://localhost:3000",
-  origin: "https://myratings.in",
+  origin: "http://localhost:3000",
+  //origin: "https://myratings.in",
   credentials: true,
   methods: ["GET", "POST", "HEAD", "PUT", "PATCH", "DELETE"],
   allowedHeaders: [
